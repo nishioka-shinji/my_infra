25a80080acf76ad7a122e3a80198723a9ccaa961
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2025-09-19 08:59:57 +0900

    domain修正

--- terraform/gcp/02-network/dns.tf
+++ terraform/gcp/02-network/dns.tf
@@ -8,8 +8,8 @@ resource "google_compute_global_address" "nishiokatest_ip" {
   name = "nishiokatest-ip"
 }
 
-resource "google_dns_record_set" "a_record" {
-  name         = google_dns_managed_zone.nishioka_test_zone.dns_name
+resource "google_dns_record_set" "a_record_wildcard" {
+  name         = "*.${google_dns_managed_zone.nishioka_test_zone.dns_name}"
   managed_zone = google_dns_managed_zone.nishioka_test_zone.name
   type         = "A"
   ttl          = 300

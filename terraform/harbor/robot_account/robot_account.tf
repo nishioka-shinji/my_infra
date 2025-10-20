9e9e2abe64a61139043ef7026da15a05ebe41242
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2025-10-20 22:22:07 +0900

    robot-account追加

--- terraform/harbor/robot_account/robot_account.tf
+++ terraform/harbor/robot_account/robot_account.tf
@@ -14,3 +14,16 @@ resource "harbor_robot_account" "github-actions-pusher" {
     }
   }
 }
+
+resource "harbor_robot_account" "k8s-puller" {
+  name   = "k8s-puller"
+  level  = "system"
+  permissions {
+    kind      = "project"
+    namespace = data.terraform_remote_state.my_project.outputs.project_name
+    access {
+      action   = "pull"
+      resource = "repository"
+    }
+  }
+}

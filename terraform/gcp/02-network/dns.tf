91aa92f216b6f6fc8fd887a65d24a6e2e0debc1e
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2026-01-26 00:34:13 +0900

    feat: remove nishiokatest network resources

--- terraform/gcp/02-network/dns.tf
+++ terraform/gcp/02-network/dns.tf
@@ -3,31 +3,3 @@ resource "google_dns_managed_zone" "akashi_rb_zone" {
   dns_name    = "akashi-rb.com."
   description = "Managed zone for akashi-rb.com"
 }
-
-resource "google_dns_managed_zone" "nishioka_test_zone" {
-  name        = "nishioka-test-zone"
-  dns_name    = "nishiokatest.xyz."
-  description = "Managed zone for nishiokatest.xyz"
-}
-
-resource "google_compute_global_address" "nishiokatest_ip" {
-  name = "nishiokatest-ip"
-}
-
-resource "google_dns_record_set" "a_record_wildcard" {
-  name         = "*.${google_dns_managed_zone.nishioka_test_zone.dns_name}"
-  managed_zone = google_dns_managed_zone.nishioka_test_zone.name
-  type         = "A"
-  ttl          = 300
-  rrdatas      = [google_compute_global_address.nishiokatest_ip.address]
-}
-
-resource "google_dns_record_set" "nishiokatest_auth_cname" {
-  name         = google_certificate_manager_dns_authorization.nishiokatest_dns_auth.dns_resource_record[0].name
-  type         = google_certificate_manager_dns_authorization.nishiokatest_dns_auth.dns_resource_record[0].type
-  ttl          = 300
-  managed_zone = google_dns_managed_zone.nishioka_test_zone.name
-  rrdatas = [
-    google_certificate_manager_dns_authorization.nishiokatest_dns_auth.dns_resource_record[0].data
-  ]
-}

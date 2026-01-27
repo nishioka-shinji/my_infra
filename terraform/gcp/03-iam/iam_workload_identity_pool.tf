015475c5acadc6c2b3aa88e33aca15bcffa01eb7
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2026-01-28 00:00:42 +0900

    refactor: use for_each for workload identity pool providers

--- terraform/gcp/03-iam/iam_workload_identity_pool.tf
+++ terraform/gcp/03-iam/iam_workload_identity_pool.tf
@@ -1,29 +1,21 @@
+locals {
+  github_owners = {
+    "akashi_rb"       = "AKASHI-rb"
+    "nishioka_shinji" = "nishioka-shinji"
+  }
+}
+
 resource "google_iam_workload_identity_pool" "github" {
   workload_identity_pool_id = "github-pool"
 }
 
-resource "google_iam_workload_identity_pool_provider" "akashi_rb" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
-  workload_identity_pool_provider_id = "akashi-rb-prvdr"
-  display_name                       = "akashi-rb-prvdr"
-  attribute_condition                = "attribute.repository_owner == \"AKASHI-rb\""
-  attribute_mapping = {
-    "google.subject"             = "assertion.sub"
-    "attribute.actor"            = "assertion.actor"
-    "attribute.aud"              = "assertion.aud"
-    "attribute.repository"       = "assertion.repository"
-    "attribute.repository_owner" = "assertion.repository_owner"
-  }
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-}
+resource "google_iam_workload_identity_pool_provider" "github_owners" {
+  for_each = local.github_owners
 
-resource "google_iam_workload_identity_pool_provider" "nishioka_shinji" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
-  workload_identity_pool_provider_id = "nishioka-shinji-prvdr"
-  display_name                       = "nishioka-shinji-prvdr"
-  attribute_condition                = "attribute.repository_owner == \"nishioka-shinji\""
+  workload_identity_pool_provider_id = "${each.key}-prvdr"
+  display_name                       = "${each.key}-prvdr"
+  attribute_condition                = "attribute.repository_owner == \"${each.value}\""
   attribute_mapping = {
     "google.subject"             = "assertion.sub"
     "attribute.actor"            = "assertion.actor"

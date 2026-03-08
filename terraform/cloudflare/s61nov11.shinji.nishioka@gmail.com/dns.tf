0d78c3931278853df9e225887ba5cc26f67725b6
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2026-03-08 22:03:33 +0900

    fix(cloudflare): remove nishiokatest.xyz domain and related records

--- terraform/cloudflare/s61nov11.shinji.nishioka@gmail.com/dns.tf
+++ terraform/cloudflare/s61nov11.shinji.nishioka@gmail.com/dns.tf
@@ -1,12 +1,3 @@
-resource "cloudflare_dns_record" "nishiokatest_xyz" {
-  zone_id = cloudflare_zone.this["nishiokatest.xyz"].id
-  name    = "*.nishiokatest.xyz"
-  type    = "CNAME"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.this.id}.cfargotunnel.com"
-  ttl     = 1
-  proxied = true
-}
-
 resource "cloudflare_dns_record" "muso_lab_dev" {
   zone_id = cloudflare_zone.this["muso-lab.dev"].id
   name    = "*.muso-lab.dev"
